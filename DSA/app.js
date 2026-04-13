7a5119df46d9c3ed1e626d1519791b992bf3f731
VJADOUN2000 <jadounvikrant@gmail.com>
2026-04-13 22:27:03 +0530

    install prompt-sync  & practice via terminal

--- DSA/app.js
+++ DSA/app.js
@@ -1,7 +1,7 @@
 
 
 var temp = "Hello World"
-console.log(temp);
+// console.log(temp);
 //temp= 100;
 
 
@@ -9,15 +9,22 @@ let s1 =" Vikrant";
 let s2 = null;
 
 
-console.log(s1 + s2);
+//console.log(s1 + s2);
 
 let a = 10;
 let b = 20;
 
 
-console.log(`The sum of ${a} and ${b} is ${a+b}`);
+// console.log(`The sum of ${a} and ${b} is ${a+b}`);
 
-//type coercion
-console.log(undefined-5);
+// //type coercion
+// console.log(undefined-5);
 
-console.log("24" %5)
+// console.log("24" %5)
+
+
+let prompt = require('prompt-sync')()
+
+let age = Number(prompt("Enter your Age "));
+
+console.log("Your correct age is: " + (age+2))
